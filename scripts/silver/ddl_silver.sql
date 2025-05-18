c9f08e69208f18157e72f07c1f9767e2e0386841
Rubén <71216800+rubenlb99@users.noreply.github.com>
2025-05-18 12:09:56 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -6,7 +6,8 @@ create table silver.crm_cust_info (
     cst_lastname        VARCHAR(50),
     cst_marital_status  CHAR(1),         -- E.g. M, S
     cst_gndr            CHAR(1),         -- E.g. M, F
-    cst_create_date     DATE
+    cst_create_date     DATE,
+	dwh_create_date 	DATE DEFAULT CURRENT_DATE
 );
 
 DROP TABLE IF EXISTS silver.crm_prd_info;
@@ -17,7 +18,8 @@ create table silver.crm_prd_info (
     prd_cost       NUMERIC(10,2),    
     prd_line       VARCHAR(10),
     prd_start_dt   DATE,
-    prd_end_dt     DATE
+    prd_end_dt     DATE,
+	dwh_create_date 	DATE DEFAULT CURRENT_DATE
 );
 
 DROP TABLE IF EXISTS silver.crm_sales_details;
@@ -30,14 +32,16 @@ create table silver.crm_sales_details (
     sls_due_dt      DATE,
     sls_sales       NUMERIC(12, 2),
     sls_quantity    INTEGER,
-    sls_price       NUMERIC(10, 2)
+    sls_price       NUMERIC(10, 2),
+	dwh_create_date 	DATE DEFAULT CURRENT_DATE
 );
 
 DROP TABLE IF EXISTS silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12 (
     cid     VARCHAR(20),
     bdate   DATE,
-    gen     VARCHAR(10)
+    gen     VARCHAR(10),
+	dwh_create_date 	DATE DEFAULT CURRENT_DATE
 );
 
 DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
@@ -45,12 +49,14 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id            VARCHAR(20),
     cat           VARCHAR(50),
     subcat        VARCHAR(50),
-    maintenance   VARCHAR(10)
+    maintenance   VARCHAR(10),
+	dwh_create_date 	DATE DEFAULT CURRENT_DATE
 );
 
 DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
     cid     VARCHAR(20),
-    cntry   VARCHAR(50)
+    cntry   VARCHAR(50),
+	dwh_create_date 	DATE DEFAULT CURRENT_DATE
 );
 
