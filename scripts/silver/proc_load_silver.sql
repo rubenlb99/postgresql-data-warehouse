0762d69b2b8d04dbcf381a09fa3d313500cbbea3
Rubén <71216800+rubenlb99@users.noreply.github.com>
2025-05-18 15:26:41 +0200

    Create proc_load_silver.sql

---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,34 @@
+INSERT INTO silver.crm_cust_info (
+    cst_id,
+    cst_key,
+    cst_firstname,
+    cst_lastname,
+    cst_marital_status,
+    cst_gndr,
+    cst_create_date
+)
+SELECT 
+    cst_id,
+    TRIM(cst_key) AS cst_key,
+    TRIM(cst_firstname) AS cst_firstname,
+    TRIM(cst_lastname) AS cst_lastname,
+    CASE
+        WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+        WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+        ELSE 'n/a'
+    END AS cst_marital_status,
+    CASE
+        WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+        WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+        ELSE 'n/a'
+    END AS cst_gndr,
+    cst_create_date
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY cst_id 
+               ORDER BY cst_create_date DESC
+           ) AS flag_last
+    FROM bronze.crm_cust_info
+) AS t
+WHERE flag_last = 1;
