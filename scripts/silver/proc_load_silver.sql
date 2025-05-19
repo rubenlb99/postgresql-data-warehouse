144f11f5802185f79ab598c3e45506c95ee27888
Rubén <71216800+rubenlb99@users.noreply.github.com>
2025-05-19 13:29:58 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -66,3 +66,38 @@ SELECT
 FROM bronze.crm_prd_info
 
 # ===============================================================================================================
+
+TRUNCATE TABLE silver.crm_sales_details;
+INSERT INTO silver.crm_sales_details (
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+)
+SELECT 
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    CASE
+        WHEN sls_sales <= 0 OR sls_sales IS NULL OR sls_sales != sls_quantity * ABS(sls_price)
+        THEN ABS(sls_price) * sls_quantity
+        ELSE sls_sales
+    END AS sls_sales,
+    sls_quantity,
+    CASE
+        WHEN sls_price < 0 THEN ABS(sls_price)
+        WHEN sls_price = 0 OR sls_price IS NULL THEN ABS(sls_sales) / sls_quantity
+        ELSE sls_price
+    END AS sls_price
+FROM bronze.crm_sales_details;
+
+# ===============================================================================================================
+
