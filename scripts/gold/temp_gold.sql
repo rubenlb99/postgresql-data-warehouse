6e0a4fc1d93024912f7fe2218aab3e7e3c0c5c15
Rubén <71216800+rubenlb99@users.noreply.github.com>
2025-05-20 11:36:56 +0200

    Update temp_gold.sql

--- scripts/gold/temp_gold.sql
+++ scripts/gold/temp_gold.sql
@@ -22,3 +22,22 @@ JOIN silver.erp_cust_az12 caz
 JOIN silver.erp_loc_a101 cloc
     ON ci.cst_key = cloc.cid;
 
+-- products dim
+
+CREATE OR REPLACE VIEW gold.dim_products AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY pi.prd_start_dt, pi.prd_key) AS product_key,
+    pi.prd_id AS product_id,
+    pi.prd_key AS product_number,
+    pi.prd_nm AS product_name,
+    pi.cat_id AS category_id,
+    cat.cat AS category,
+    cat.subcat AS subcategory,
+    cat.maintenance AS maintenance,
+    pi.prd_cost AS cost,
+    pi.prd_line AS product_line,
+    pi.prd_start_dt AS start_date
+FROM silver.crm_prd_info pi
+JOIN silver.erp_px_cat_g1v2 cat
+    ON pi.cat_id = cat.id
+WHERE pi.prd_end_dt IS NULL; --filter historical data
