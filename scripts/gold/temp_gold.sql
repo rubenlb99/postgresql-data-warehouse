fe205861786eafe371ef2c0f08c2bd906f6952f8
Rubén <71216800+rubenlb99@users.noreply.github.com>
2025-05-20 11:15:28 +0200

    Update temp_gold.sql

--- scripts/gold/temp_gold.sql
+++ scripts/gold/temp_gold.sql
@@ -1,17 +1,24 @@
 -- customer dim
 
-select 
-	ci.cst_id as CUSTOMER_ID,
-	ci.cst_key AS CUSTOMER_KEY,
-	ci.cst_firstname AS FIRSTNAME,
-	ci.cst_lastname AS LASTNAME,
-	ci.cst_marital_status AS MARITAL_STATUS,
-	ci.cst_gndr AS GENDER, 
-	caz.bdate AS BIRTHDAY,
-	cloc.cntry AS COUNTRY,
-	ci.cst_create_date AS CREATED_DATE
-from silver.crm_cust_info ci
-join silver.erp_cust_az12 caz
-on ci.cst_key = caz.cid
-join silver.erp_loc_a101 cloc
-on ci.cst_key = cloc.cid
+CREATE OR REPLACE VIEW gold.dim_customer AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key,
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS firstname,
+    ci.cst_lastname AS lastname,
+    cloc.cntry AS country,
+    CASE 
+        WHEN ci.cst_gndr != 'n/a'
+        THEN ci.cst_gndr
+        ELSE COALESCE(caz.gen, 'n/a')
+    END AS gender,
+    caz.bdate AS birthdate,
+    ci.cst_marital_status AS marital_status,
+    ci.cst_create_date AS created_date
+FROM silver.crm_cust_info ci
+JOIN silver.erp_cust_az12 caz
+    ON ci.cst_key = caz.cid
+JOIN silver.erp_loc_a101 cloc
+    ON ci.cst_key = cloc.cid;
+
